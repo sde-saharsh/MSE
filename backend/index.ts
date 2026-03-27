cecd90b7db3076b3079de27e60f9a3f459a9ea77
Saharshx <saharshkhalokar14@gmail.com>
2026-03-27 18:39:39 +0530

    cors

--- backend/index.ts
+++ backend/index.ts
@@ -10,7 +10,9 @@ import analyticsRoutes from './routes/analytics';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
